92b8b2677ceb8e5ea65c124a3c23682abc2d97c4
Jared E Plummer <113934345+JaredPlummer5@users.noreply.github.com>
2023-05-18 20:00:48 -0500

    Caching is Complete

--- src/weather.js
+++ src/weather.js
@@ -1,6 +1,8 @@
 const axios = require('axios');
+const NodeCache = require("node-cache");
+const forecastCache = new NodeCache();
 
-exports.forcast = function (req, res) {
+exports.forecast = function (req, res) {
     // Define a Forecast class to represent weather forecast data
     class Forecast {
         constructor(date, description, lon, lat, city_name, id) {
@@ -12,19 +14,30 @@ exports.forcast = function (req, res) {
             this.id = id;
         }
     }
+
     const { city } = req.query;
     const api = 'eb1a646a8644433db25fe7d36799fb2a';
     const url = `https://api.weatherbit.io/v2.0/forecast/daily?city=${city}&key=${api}`;
+    const cacheKey = `${city}`;
+    let forecastCacheData = forecastCache.get(`${cacheKey}`);
 
-    // Make an API request to get weather data for the specified city
-    let weatherData = axios.get(url)
-        .then(response => {
-            // Map the weather data response to create an array of Forecast objects
-            let mappingWeatherData = response.data.data.map(element => {
-                let newForcast = new Forecast(element.datetime, element.weather.description, response.data.lon, response.data.lat, response.data.city_name);
-                return newForcast;
-            });
-            res.send(mappingWeatherData); // Respond with the mapped weather forecast data
-        })
-        .catch(error => res.send(error.message)); // Handle any errors that occur during the API request
-}
+    if (forecastCacheData) {
+        res.send(forecastCacheData);
+        console.log("CacheData", forecastCacheData);
+    } else {
+        console.log('New data');
+        // Make an API request to get weather data for the specified city
+        let weatherData = axios.get(url)
+            .then(response => {
+                // Map the weather data response to create an array of Forecast objects
+                let mappingWeatherData = response.data.data.map(element => {
+                    let newForecast = new Forecast(element.datetime, element.weather.description, response.data.lon, response.data.lat, response.data.city_name);
+                    return newForecast;
+                });
+                res.send(mappingWeatherData); // Respond with the mapped weather forecast data
+                const ttl = 3600; // TTL in seconds (e.g., 1 hour)
+                forecastCache.set(cacheKey, mappingWeatherData, ttl);
+            })
+            .catch(error => res.send(error.message)); // Handle any errors that occur during the API request
+    }
+};
